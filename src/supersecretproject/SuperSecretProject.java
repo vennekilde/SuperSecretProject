a3409752629f686bb205b1126d9523711ce06764
Jeppe Vennekilde <jeppe@jeppes.info>
2012-11-28 11:24:10 +0100

    Removed unused License

--- src/supersecretproject/SuperSecretProject.java
+++ src/supersecretproject/SuperSecretProject.java
@@ -5,36 +5,6 @@ import info.jeppes.ZoneCore.ZonePlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import supersecretproject.Util.MSG;
 
-/*
- * Author: Jeppe Boysen Vennekilde
- *
- * This document is Copyright ©() and is the intellectual property of the author.
- *
- * TERMS AND CONDITIONS
- * 0. USED TERMS
- * OWNER - The original author(s) of the program
- * USER - End user of the program, person installing/using the program.
- *
- * 1. LIABILITY
- * THIS PROGRAM IS PROVIDED 'AS IS' WITH NO WARRANTIES, IMPLIED OR OTHERWISE.
- * THE OWNER OF THIS PROGRAM TAKES NO RESPONSIBILITY FOR ANY DAMAGES INCURRED
- * FROM THE USE OF THIS PROGRAM.
- *
- * 2. REDISTRIBUTION
- * This program may only be distributed where uploaded, mirrored, or otherwise
- * linked to by the OWNER solely. All mirrors of this program must have advance
- * written permission from the OWNER. ANY attempts to make money off of this
- * program (selling, selling modified versions, adfly, sharecash, etc.) are
- * STRICTLY FORBIDDEN, and the OWNER may claim damages or take other action to
- * rectify the situation.
- *
- * 3. DERIVATIVE WORKS/MODIFICATION
- * This program is provided freely and may be decompiled and modified for
- * private use, either with a decompiler or a bytecode editor. Public
- * distribution of modified versions of this program require advance written
- * permission of the OWNER and may be subject to certain terms.
- */
-
 public class SuperSecretProject extends ZonePlugin{
     private Loader loader;
 
